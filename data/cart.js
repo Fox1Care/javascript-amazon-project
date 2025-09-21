a4cb33d50de829903bd969e6c38f518b925cd802
F1C <dovnar.il@yandex.ru>
2025-09-21 23:42:04 +0300

    save cart in localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,10 +1,18 @@
-export let cart = [{
+export let cart = JSON.parse(localStorage.getItem('cart')) 
+if(cart.length  === 0) {
+  cart = 
+[{
   productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
   quantity: 2,
 }, {
   productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
   quantity: 1,
 }];
+}
+
+function saveToStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 export function addToCart(productId) {
   let matchingItem;
@@ -25,6 +33,8 @@ export function addToCart(productId) {
       quantity: Number(selectObject.value),
     });
   }
+
+  saveToStorage();
 }
 
 export function removeFromCart(productId) {
@@ -37,4 +47,7 @@ export function removeFromCart(productId) {
   });
 
 cart = newCart;
+console.log(cart);
+
+saveToStorage();
 }
